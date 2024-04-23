910951b3a8de009b5a4b8a2453f1d21b12e344fe
borox <77976959+borox345@users.noreply.github.com>
2024-04-23 12:17:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 # USING CRAMER FORMULAS
 def get_equations():
-  print("First equation. (Format ax + by = c):")
-  eq1 = input("1: ").split()
-  print("Second equation. (Format ax + by = c):")
-  eq2 = input("2: ").split()
-  
+    print("First equation. (Format ax + by = c):")
+    eq1 = input("1: ").replace("x", " ").replace("y", " ").split()
+    print("Second equation. (Format ax + by = c):")
+    eq2 = input("2: ").replace("x", " ").replace("y", " ").split()
 
+    print(eq1)
+    print(eq2)
 
-  equ1 = (int(eq1[0][0]), int(eq1[2][0]), int(eq1[4]))
-  equ2 = (int(eq2[0][0]), int(eq2[2][0]), int(eq2[4]))
-  
-  return equ1, equ2
+    equ1 = (int(eq1[0][0]), int(eq1[2][0]), int(eq1[4]))
+    equ2 = (int(eq2[0][0]), int(eq2[2][0]), int(eq2[4]))
+    
+    return equ1, equ2
 
 def cramer_formulas(a1, b1, c1, a2, b2, c2):
     determinant = a1 * b2 - a2 * b1
@@ -33,13 +34,26 @@ def cramer_formulas(a1, b1, c1, a2, b2, c2):
 #   print("Equations are parallel.")
 
 if __name__ == "__main__":
-  option = input("Option 1/2: ")
-  if option == 1:
-    input = get_equations()
-    solution = cramer_formulas(*input[0], *input[1])
-    if solution:
-      print("x =", solution[0], ", y =", solution[1])
-    else:
-      print("Equations are parallel.")
-  elif option == 2:
-    print("s")
+    option = int(input("Option 1/2: "))
+    if option == 1:
+        input = get_equations()
+    elif option == 2:
+        print("Give numbers for X:")
+        print("FORMAT: x = A*x + B*y = c")
+        x_input_a1 = int(input("A number: "))
+        x_input_b1 = int(input("B number: "))
+        x_input_c1 = int(input("C number: "))
+        x_nums = (x_input_a1, x_input_b1, x_input_c1)
+
+        print("Give numbers for Y:")
+        print("FORMAT: y= A*x + B*y = c")
+        y_input_a1 = int(input("A number: "))
+        y_input_b1 = int(input("B number: "))
+        y_input_c1 = int(input("C number: "))
+        y_nums = (y_input_a1, y_input_b1, y_input_c1)
+        
+        sol = cramer_formulas(*x_nums, *y_nums)
+        if sol:
+            print("x =", sol[0], ", y =", sol[1])
+        else:
+            print("Equations are parallel.")
